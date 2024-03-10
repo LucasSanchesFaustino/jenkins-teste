bb886e26422727967784526a78afdf87c2eacceb
Lucas <lucassanchesfaustino3@hotmail.com>
2024-03-10 01:57:11 -0300

    Testing Sonar quality gate

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.awt.*;
+
 public class Calculator {
 
 	
@@ -11,6 +13,14 @@ public class Calculator {
  	public float subtract (float number1, float number2) {
  		
  		return (number1 - number2);
+ 	}
+ 	
+ 	public boolean sonarTest(String test) {
+ 	    return false;
+ 	}
+
+ 	public void processData(String test) {
+ 	    sonarTest(test);
  	}
 
 
